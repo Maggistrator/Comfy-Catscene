a7c3d1d7a90d768867fb4ab5c3cde9061ac90fea
Maggistrator <lira_hertstrings@mail.ru>
2020-04-25 23:58:00 +0300

    published libs

--- src/ui/general/KindOfWindow.java
+++ src/ui/general/KindOfWindow.java
@@ -7,15 +7,25 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.Menu;
+import java.awt.MenuBar;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.lang.reflect.InvocationTargetException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
 import javax.swing.border.EtchedBorder;
 import org.newdawn.slick.SlickException;
 import ui.player.SlickSandbox;
@@ -24,26 +34,42 @@ import ui.player.SlickSandbox;
  * НАПОМИНАЛКА: Использовать для редактора XML библиотеку rsyntaxtextarea 
  * из BeardedSorcererIDE, чтобы она была сразу с подсветкой синтаксиса
  * 
- * @author Sova
+ * @author Maggistrator
  */
 public class KindOfWindow {    
-    JFrame frame = new JFrame("Comfy Catscene");    //Главное окно программы
+    JFrame frame;    //Главное окно программы
     Player player;                                  //Slick2D-холст предпросмотра
-    Hiline hiline = new Hiline();                   //Hilight+Timline - имитатор таймлайна
+    Hiline hiline;                   //Hilight+Timline - имитатор таймлайна
+    SlickSandbox game;//Slick2D-игра, управляющая нативной отрисовкой Slick2D-контейнера "player"
+    JMenuBar menu ;
+    
+    @Deprecated
     JPanel modePanel = new JPanel();                //Панель управления режимами программы
+    @Deprecated
     JPanel fileFunctionsPanel = new JPanel();       //Панель файловых операций, таких как сохранение или загрузка 
-    SlickSandbox game = new SlickSandbox("Sandbox");//Slick2D-игра, управляющая нативной отрисовкой Slick2D-контейнера "player"
 
     
     public KindOfWindow() {
+        frame = new JFrame("Comfy Catscene");
+        hiline = new Hiline(); 
+        menu = new JMenuBar();
+        game = new SlickSandbox("Sandbox");
         //Плеер начинает отрисовку с момента старта программы
-        try {
-            player = new Player(game);
-            player.play();
-        } catch (SlickException ex) {
-            JOptionPane.showMessageDialog(frame, "Slick не работает, предпросмотр отвалился");
-        }
-        
+        SwingWorker paralelle = new SwingWorker() {
+            @Override
+            protected Object doInBackground() throws Exception {
+            try {
+                player = new Player(game);
+                return player;
+            } catch (SlickException | NullPointerException ex) {
+                JOptionPane.showMessageDialog(frame, "Slick не работает, предпросмотр отвалился");
+            }
+                return null;
+            }
+        };
+        if(player != null) 
+                player.play();
+        else System.out.println("suck my cock");
         //Настройка параметров окна
         frame.setSize(640, 480);
         frame.setLocationRelativeTo(null);
@@ -72,6 +98,7 @@ public class KindOfWindow {
                 System.exit(0);
             }
         });
+        buildMainMenu();
         configureLayout();
     }
     
@@ -96,6 +123,27 @@ public class KindOfWindow {
         fileFunctionsPanel.add(imp);
     }
     
+    private void buildMainMenu(){
+        JMenu fileMenuItem = new JMenu("File");
+        JMenu imageMenuItem = new JMenu("Image");
+        JMenu musicMenuItem = new JMenu("Music");
+        JMenu soundMenuItem = new JMenu("Sound");
+        JMenu subtitlesMenuItem = new JMenu("Subtitles");
+        JMenu modeMenuItem = new JMenu("Mode");
+        JMenu helpMenuItem = new JMenu("Help");
+        
+        menu.add(fileMenuItem);
+        menu.add(imageMenuItem);
+        menu.add(musicMenuItem);
+        menu.add(soundMenuItem);
+        menu.add(subtitlesMenuItem);
+        menu.add(modeMenuItem);
+        menu.add(helpMenuItem);
+       
+        //menu.setHelpMenu(helpMenuItem); //Not yet implemented in JDK
+        frame.setJMenuBar(menu);
+    }
+    
     private void configureLayout(){
         //Расположение плеера
         GridBagConstraints c = new GridBagConstraints();
@@ -115,7 +163,7 @@ public class KindOfWindow {
         mpc.gridy = 1;
         mpc.weightx = 0.0;
         mpc.insets = new Insets(0, 10, 10, 0);
-        frame.add(modePanel, mpc);
+        //frame.add(modePanel, mpc);
         
         //Конфигурируем таймлайн
         GridBagConstraints tlpc = new GridBagConstraints();
@@ -124,7 +172,7 @@ public class KindOfWindow {
         tlpc.gridy = 1;
         tlpc.weightx = 0.8;
         tlpc.weighty = 0.1;
-        tlpc.insets = new Insets(0, 0, 10, 0);
+        tlpc.insets = new Insets(0, 10, 10, 10);
         frame.add(hiline, tlpc);
         
         //Конфигурируем панель файловых функций
@@ -134,7 +182,7 @@ public class KindOfWindow {
         ffpc.gridx = 2;
         ffpc.gridy = 1;
         ffpc.insets = new Insets(0, 0, 10, 10);
-        frame.add(fileFunctionsPanel, ffpc);
+        //frame.add(fileFunctionsPanel, ffpc);
     }
     
     /**
